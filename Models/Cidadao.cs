cd5ece44903a3d040fee11b8e28f253daea78515
Ismael Reis <sidimsolucoes@outlook.com>
2024-07-18 15:07:08 -0300

    add: list cidadao

--- Models/Cidadao.cs
+++ Models/Cidadao.cs
@@ -11,6 +11,23 @@ namespace SidimEsus.Models
         public int Id { get; set; }
         [Column("no_cidadao")]
         public string Nome { get; set; }
+        [Column("nu_cns")]
+        public string Cns { get; set; }
+        [Column("nu_cpf_cidadao")]
+        public string Cpf { get; set; }
+        [Column("st_faleceu")]
+        public int? Faleceu { get; set; }
+        [Column("co_dim_sexo")]
+        public int? Sexo { get; set; }
+        [Column("nu_telefone_celular")]
+        public string telefone { get; set; }
+        [Column("co_dim_tempo_nascimento")]
+        public int? nascimento { get; set; }
+        [Column("co_dim_unidade_saude_vinc")]
+        public int? EstabelecimentoId { get; set; }
+        [Column("co_dim_equipe_vinc")]
+        public int? EquipeId { get; set; }
+
 
     }
 }
